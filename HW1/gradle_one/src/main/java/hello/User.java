8c53cb89269d79d73df8e5b0c4622bff7095c08d
Patrick Boucicault <patrickjoshuab@gmail.com>
2019-02-25 23:27:14 -0500

    Updated some of the new classes. Created a controller class for the user. Still need to set up the mongo server and some crud stuff

--- HW1/gradle_one/src/main/java/hello/User.java
+++ HW1/gradle_one/src/main/java/hello/User.java
@@ -1,6 +1,7 @@
 package hello;
 import java.util.List;
 
+@Document(collection = "Users")
 public class User{
 
     @Id
@@ -8,7 +9,49 @@ public class User{
     String userName;
     String password;
 
-    ArrayList<Games>;
+    ArrayList<Games> games;
+
+    public User(String id, String userName, String password)
+    {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+
+        games = new ArrayList<Games>();
+
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public void setUsername(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setPasswordname(String passwordName)
+    {
+        this.passwordName = passwordName;
+    }
+
+    public String getPasswordName()
+    {
+        return password;
+    }
+
+
 
 
 
